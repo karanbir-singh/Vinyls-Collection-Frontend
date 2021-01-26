ccdc02ba291fa0bcd3a9a0a6c7670f218383599b
karanbir-singh <karans1723@gmail.com>
2021-01-26 21:37:42 +0100

    Worked on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,69 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles.css";
-import { Button } from "react-bootstrap";
+import { Alert, Button } from "react-bootstrap";
+
+const possibilities = [
+    {
+        method: "GET",
+        uri: "/vinyls",
+        variant: "success"
+    },
+    {
+        method: "GET",
+        uri: "/authors",
+        variant: "success"
+    },
+    {
+        method: "GET",
+        uri: "/genres",
+        variant: "success"
+    },
+    {
+        method: "GET",
+        uri: "/vinyls/:id",
+        variant: "success"
+    },
+    {
+        method: "GET",
+        uri: "/authors/:id/vinyls",
+        variant: "success"
+    },
+    {
+        method: "GET",
+        uri: "/genres/:id/vinyls",
+        variant: "success"
+    },
+    {
+        method: "POST",
+        uri: "/vinyls",
+        variant: "info"
+    },
+    {
+        method: "POST",
+        uri: "/authors",
+        variant: "info"
+    },
+    {
+        method: "POST",
+        uri: "/genres",
+        variant: "info"
+    },
+]
+
 export function Home(props) {
     return (
-        <>
-            <Button variant="primary">Prova React Boostrap</Button>
-        </>
+        <div style={{ display: "inline-block", margin: "10px" }}>
+            {
+                possibilities.map((req) => {
+                    return (<Alert variant={req.variant}>
+                        <Button className="get-post-btn" variant={req.variant}>{req.method}</Button>
+                        <span className="alert-span">{req.uri}</span>
+                        <Button style={{ float: "right", marginLeft: "80px" }} variant={"outline-" + req.variant}>{req.method}</Button>
+                    </Alert>);
+                })
+            }
+        </div>
+
     );
 }
---
+++ src/components/VinylsList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../styles.css";
+
+export function VinylsList() {
+    return(
+        <></>
+    );
+}
